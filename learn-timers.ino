d84bea2b8ff24822eee92e2d0741e27caeb9aa34
peteGSX <peteracole@outlook.com.au>
2023-02-10 05:30:00 +1000

    More ref info

--- learn-timers.ino
+++ learn-timers.ino
@@ -1,7 +1,13 @@
 #include <Arduino.h>
 #include "timer_functions.h"
 
-// Ref: https://docs.arduino.cc/tutorials/generic/secrets-of-arduino-pwm
+/*
+Ref: https://docs.arduino.cc/tutorials/generic/secrets-of-arduino-pwm
+Timer ref: https://arbaranwal.github.io/tutorial/2017/06/23/atmega328-register-reference.html
+PCA9685 set to 50Hz clock
+
+Looks like we need to adjust OCRnA/B to set duty cycle
+*/
 
 void setup() {
   setupTimers();
