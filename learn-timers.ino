ae57f8e25af68a4598c78f449f8fb4d82ecdc0c0
peteGSX <peteracole@outlook.com.au>
2023-02-16 05:29:06 +1000

    Update notes

--- learn-timers.ino
+++ learn-timers.ino
@@ -7,24 +7,19 @@ Refs:
 https://docs.arduino.cc/tutorials/generic/secrets-of-arduino-pwm
 https://arbaranwal.github.io/tutorial/2017/06/23/atmega328-register-reference.html
 https://wolles-elektronikkiste.de/en/timer-and-pwm-part-1-8-bit-timer0-2
-PCA9685 set to 50Hz clock
 
-We need to enable a full 16bit PWM signal at 50Hz, so count 0 - 65535 50 times a second.
+Use timer 2, clock speed doesn't matter, pure duty cycle based PWM signal output for LEDs only.
 
-Value being sent is the "on" part of the duty cycle.
+Maybe 10000 max = 100.00% effectively
 */
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_pin, OUTPUT);
   digitalWrite(LED_pin, LOW);
-  setupTimers();
+  // setupTimers();
 }
 
 void loop() {
-  processSerialInput();
-  // if (count == pwm) {
-  //   flash();
-  //   count = 0;
-  // }
+  
 }
