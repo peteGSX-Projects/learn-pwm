ea32815e4a42c6d328b80b60b3180a925d035ac9
peteGSX <peteracole@outlook.com.au>
2023-02-18 19:57:02 +1000

    Lost

--- avr/EXIODimmer.cpp
+++ avr/EXIODimmer.cpp
@@ -8,25 +8,47 @@
 */
 static Dimmer dimmers[MAX_DIMMERS];
 uint8_t dimmerCount = 0;
-static volatile int8_t Channel;
+// static volatile int8_t Channel;
+static volatile int8_t dimmer = 0;
 
 /*
   Static functions
 */
 static inline void handle_interrupts(volatile uint8_t *TCNTn, volatile uint8_t *OCRnA) {
+  if (dimmer < 0) {
+    *TCNTn = 0;
+  // } else {
+  //   if (dimmers[dimmer].isActive) {
+  //     digitalWrite(dimmers[dimmer].physicalPin, LOW);
+  //   }
+  }
+  dimmer++;
+  if (dimmer < MAX_DIMMERS) {
+    *OCRnA = dimmers[dimmer].onValue;
+    if (dimmers[dimmer].isActive && *TCNTn <= dimmers[dimmer].onValue) {
+      digitalWrite(dimmers[dimmer].physicalPin, HIGH);
+    } else if (dimmers[dimmer].isActive) {
+      digitalWrite(dimmers[dimmer].physicalPin, LOW);
+    }
+  } else {
+    dimmer = -1;
+  }
+  // Serial.println(counter);
   // if (Channel < 0) {
   //   *TCNTn = 0;
   // } else {
-  for (uint8_t Channel = 0; Channel < MAX_DIMMERS; Channel++) {
-    if (dimmers[Channel].isActive) {
-      *OCRnA = dimmers[Channel].onValue;
-      if (*TCNTn <= *OCRnA) {
-        digitalWrite(dimmers[Channel].physicalPin, HIGH);
-      } else {
-        digitalWrite(dimmers[Channel].physicalPin, LOW);
-      }
-    }
-  }
+  // for (uint8_t counter = 0; counter <= 255; counter ++) {
+    // for (uint8_t i = 0; i < MAX_DIMMERS; i++) {
+    //   if (dimmers[i].isActive) {
+    //     // *OCRnA = dimmers[i].onValue;
+    //     if (counter <= dimmers[i].onValue) {
+    //       digitalWrite(dimmers[i].physicalPin, HIGH);
+    //     } else {
+    //       digitalWrite(dimmers[i].physicalPin, LOW);
+    //     }
+    //   }
+    // }
+  // }
   // if (Channel < MAX_DIMMERS) {
   //   Channel++;
   // } else {
@@ -38,12 +60,20 @@ SIGNAL (TIMER2_COMPA_vect) {
   handle_interrupts(&TCNT2, &OCR2A);
 }
 
+// SIGNAL (TIMER2_OVF_vect) {
+//   handle_interrupts(&TCNT2);
+// }
+
 static void initISR() {
-  TCCR2A = (1<<WGM20);  // Phase correct PWM
-  TCCR2B = (1<<CS21) + (1<<CS22);   // Prescaler 256
+  TCCR2A = 0x00;
+  // TCCR2A = (1<<WGM20);  // Phase correct PWM
+  TCCR2B = (1<<CS21);
+  // TCCR2B = (1<<CS21) + (1<<CS22);   // Prescaler 256
   TCNT2 = 0;
   TIFR2 = (1<<OCF2A);   // Clear pending interrupts
   TIMSK2 = (1<<OCIE2A); // Interrupt when hitting OCR2A
+  // OCR2A = 1;
+  // TIMSK2 = (1<<TOIE2);
 }
 
 static bool isTimerActive(uint8_t channel) {
